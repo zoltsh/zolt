4cb64553c0430bfc9361cfb79dc3d19beb598426
zsumz <shawn@zsumz.com>
2026-08-21 16:30:51 -0500

    test(build): author fixtures in final manifest language

--- modules/zolt-build/src/test/java/sh/zolt/doctor/SelfHostingCheckServiceTest.java
+++ modules/zolt-build/src/test/java/sh/zolt/doctor/SelfHostingCheckServiceTest.java
@@ -26,14 +26,14 @@ final class SelfHostingCheckServiceTest {
                 name = "zoltish"
                 version = "0.1.0"
                 group = "sh.zolt"
-                java = "21"
+                java = 21
                 main = "sh.zolt.Main"
 
                 [test.sources]
                 java = []
                 groovy = ["src/test/groovy"]
 
-                [test.dependencies]
+                [dependencies.test]
                 "org.junit.platform:junit-platform-console-standalone" = "1.11.4"
 
                 [native]
@@ -68,10 +68,13 @@ final class SelfHostingCheckServiceTest {
                 name = "demo"
                 version = "0.1.0"
                 group = "com.example"
-                java = "21"
+                java = 21
 
-                [test.dependencies]
-                "org.junit.platform:junit-platform-console-standalone" = {}
+                [platforms]
+                "org.junit:junit-bom" = "5.13.4"
+
+                [dependencies.test]
+                "org.junit.platform:junit-platform-console-standalone" = { managed = true }
                 """);
 
         SelfHostingCheckResult result = new SelfHostingCheckService().check(projectDir);
